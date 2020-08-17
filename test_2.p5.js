c3e1310981e888cc438818e068e7ea2ef9486e23
saraHeitz <sarahelenaheitz@googlemail.com>
2020-08-17 12:39:37 +0200

    This is the first real test

--- test_2.p5.js
+++ test_2.p5.js
@@ -1,2 +1,12 @@
-print("What up?")
- 
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(255, 25, 25);
+  } else {
+    fill(255, 255, 100);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
